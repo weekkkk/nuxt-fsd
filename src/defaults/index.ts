3e5db389c884dac19aa3cdae0bba3053d1964865
Nikita Nedelko <93500964+weekkkk@users.noreply.github.com>
2025-06-24 17:04:47 +0300

    KkuQflGn: Create module defaults

---
+++ src/defaults/index.ts
@@ -0,0 +1,1 @@
+export { DEFAULT_MODULE_OPTIONS } from "./moduleOptions";
---
+++ src/defaults/layers.ts
@@ -0,0 +1,17 @@
+import type { TLayers } from "../types";
+
+export const DEFAULT_LAYERS: TLayers = {
+  shared: {},
+  entities: {
+    hasSlices: true,
+  },
+  features: {
+    hasSlices: true,
+  },
+  widgets: {
+    hasSlices: true,
+  },
+  pages: {
+    hasSlices: true,
+  },
+};
